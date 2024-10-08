2e8b6171def484b7bb8cb16887aee27e814e3fd7
Igor <4430704@gmail.com>
2024-10-08 11:13:59 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const list = [
+    ...document.querySelectorAll(".population")
+];
+const totalNode = document.querySelector(".total-population");
+const averageNode = document.querySelector(".average-population");
+const numberFormat = (number)=>new Intl.NumberFormat("en-US").format(number);
+const total = list.reduce((acc, item)=>acc + Number(item.textContent.replaceAll(",", "")), 0);
+const average = Math.round(total / list.length);
+totalNode.textContent = numberFormat(total);
+averageNode.textContent = numberFormat(average);
+
+//# sourceMappingURL=index.f75de5e1.js.map
